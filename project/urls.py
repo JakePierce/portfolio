c42d57f55fecee140f7eb860f7b2589ff0392645
Jacob Pierce <Jake@Jacobs-MacBook-Pro.local>
2015-12-16 14:57:34 -0700

    updated version of my portfolio

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^home_view/$', 'main.views.home_view'),
+    url(r'^$', 'main.views.home_view'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
